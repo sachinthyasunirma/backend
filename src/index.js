803b432a2f85a5f445c38502442aa0ab409dd858
sachinthya <ssrathnavibushana@gmail.com>
2026-01-04 17:24:30 +0530

    setup the crud operation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const { ServerConfig, Logger } = require("./config");
+const { ServerConfig } = require("./config");
 const apiRoute = require("./routes");
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", apiRoute);
 
 app.listen(ServerConfig.PORT, () => {
   console.log(`server has started on port ${ServerConfig.PORT}`);
-  Logger.info("successufully started the server");
 });
